d73e81ba8951dcb8f6f3efcd5814c8e9fb6e0e4d
hungha102 <hungha523@gmail.com>
2022-08-30 22:43:48 +0700

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Path path = Paths.get("D:/test3/ta/a.txt");
+        try {
+            Files.createDirectories(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        File file = new File("D:/test/ta/ha.txt");
+//        file.mkdirs();
+
+    }
+}
